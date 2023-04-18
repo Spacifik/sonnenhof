ce3084fcce826140426af4522b05521721ced9b7
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-04-18 23:17:45 +0200

    main: add button variants

--- src/pages/components/button.tsx
+++ src/pages/components/button.tsx
@@ -3,15 +3,16 @@ import { v4 } from "uuid";
 
 interface ButtonProps {
   label: string;
+  variant: "primary" | "secondary";
   id?: string;
   onClick?: Callback<void>;
 }
 
-export function Button({ label, id, onClick }: ButtonProps): JSX.Element {
+export function Button({ label, variant, id, onClick }: ButtonProps): JSX.Element {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const buttonId = React.useMemo(() => id ?? v4(), []);
   return (
-    <div className="button-wrapper">
+    <div className={`button-wrapper ${variant}`}>
       <label htmlFor={buttonId}>{label}</label>
       <button id={buttonId} onClick={() => onClick?.()} />
     </div>
