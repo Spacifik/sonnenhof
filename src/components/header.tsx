d9be36fa709766c4348a038d9bd38b89ab2d9c05
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-04-26 17:16:20 +0200

    main: header restyle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,40 +1,43 @@
 /* eslint-disable @next/next/no-img-element */
 import { useOverlay } from "@sonnenhof/overlay/overlay-context";
-import { LogoNoTextSvg } from "@sonnenhof/svg/logo-no-text";
-import { LogoTextSvg } from "@sonnenhof/svg/logo-text";
 import { ArrowLeft, Menu } from "iconoir-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { HamburgerOverlay } from "./hamburger-overlay";
 import { getThemeColor } from "./theme-color";
-import Image from "next/image";
 import { Logo } from "./logo";
 
 export function Header(): JSX.Element {
   const router = useRouter();
   const { showOverlay } = useOverlay();
   return (
-    <header className="sticky w-full top-0 z-30 relative">
-      {router.route !== "/" ? (
-        <Link
-          className={`cursor-pointer absolute top-3 left-3 h-1/2 ${getThemeColor(
+    <header className="w-full z-20 sticky bg-black">
+      <div className="flex flex-row">
+        {router.route !== "/" ? (
+          <Link
+            className={`cursor-pointer h-1/2 ${getThemeColor(
+              "primary"
+            )} p-3 md:p-5`}
+            href={"/"}
+          >
+            <ArrowLeft />
+          </Link>
+        ) : (
+          <div className="h-1/2 ">
+            <ArrowLeft className="invisible" />
+          </div>
+        )}
+        <div className="flex w-44 md:w-56 mx-3 md:mx-8">
+          <Logo className="h-full w-auto" />
+        </div>
+        <div
+          className={`cursor-pointer h-1/2 ${getThemeColor(
             "primary"
-          )}`}
-          href={"/"}
+          )} flex-grow justify-end flex p-3 md:p-5`}
+          onClick={() => showOverlay(<HamburgerOverlay />)}
         >
-          <ArrowLeft />
-        </Link>
-      ) : null}
-      <div
-        className={`cursor-pointer absolute top-3 right-3 h-1/2 ${getThemeColor(
-          "primary"
-        )}`}
-        onClick={() => showOverlay(<HamburgerOverlay />)}
-      >
-        <Menu />
-      </div>
-      <div className="flex w-44 md:w-56 mx-3 md:mx-8 absolute">
-        <Logo className="h-full w-auto" />
+          <Menu />
+        </div>
       </div>
     </header>
   );
