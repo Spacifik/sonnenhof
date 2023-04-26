56c1c3c8596812700cffe08cf36254c2204e73c2
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-04-26 17:46:32 +0200

    main: some custom styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@sonnenhof/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@sonnenhof/styles/globals.css";
+import "@sonnenhof/styles/datepicker.css";
+import type { AppProps } from "next/app";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
