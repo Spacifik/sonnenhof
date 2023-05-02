427efc31f4997e053a2e765e15f09dec4e64fb9b
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-05-02 20:19:45 +0200

    main: plug in title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon_logo.svg" type="image/svg+xml" />
+        <title>Willkommen auf dem Sonnenhof</title>
       </Head>
       <body>
         <Main />
