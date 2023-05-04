fe6c5605f45a394a19dafd21c399f1a94f75f14c
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-05-04 17:34:34 +0200

    main: plug in the structure for the generic pages

--- src/components/empty-page.tsx
+++ src/components/empty-page.tsx
@@ -1,3 +1,47 @@
-export function EmptyPage(): JSX.Element {
-  return <div className="min-h-60vh"></div>;
+// components/DummyPage.tsx
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Text } from "./text";
+
+interface EmptyPage {
+  title: string;
+  description: string;
+  content: string;
 }
+
+export const EmptyPage: React.FC = () => {
+  const [textData, setTextData] = useState<EmptyPage | null>(null);
+  const router = useRouter();
+  const { pathname } = router;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { default: data } = await import(`../data${pathname}/text`);
+        setTextData(data);
+      } catch (error) {
+        console.error("Error fetching text data:", error);
+      }
+    };
+
+    fetchData();
+  }, [pathname]);
+
+  if (!textData) {
+    return <div>Loading...</div>;
+  }
+
+  const { title, description, content } = textData;
+  return (
+    <div className="flex flex-col gap-5 md:gap-8 lg:gap-12 bg-black">
+      <div className="flex flex-col gap-1">
+        <Text variant="tiny-primary" mono uppercase>
+          {description}
+        </Text>
+        <Text variant="big-primary">{title}</Text>
+      </div>
+
+      <Text variant="small-primary">{content}</Text>
+    </div>
+  );
+};
---
+++ src/data/activities/text.ts
@@ -0,0 +1,8 @@
+const text = {
+  title: "Activities",
+  description: "Activity description",
+  content:
+    "This is where life happens and shows its truly beautiful colors and shapes",
+};
+
+export default text;
