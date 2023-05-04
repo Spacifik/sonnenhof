57e6d2064ba299621757225cd35b78493cee91a6
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-05-04 17:35:54 +0200

    main: add padding

--- src/data/activities/text.ts
+++ src/data/activities/text.ts
@@ -2,7 +2,7 @@ const text = {
   title: "Activities",
   description: "Activity description",
   content:
-    "This is where life happens and shows its truly beautiful colors and shapes",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum euismod velit at malesuada. Cras mollis nunc varius urna aliquet gravida. Nulla facilisi. Donec placerat ligula ultricies est pulvinar suscipit at sed felis. Integer porta egestas sodales. Integer dictum, nunc nec luctus lobortis, velit sem fringilla leo, id consectetur neque magna vitae diam. Sed suscipit molestie auctor. Ut eu lacus non velit ultrices malesuada quis sed arcu. Morbi ut magna metus.",
 };
 
 export default text;
