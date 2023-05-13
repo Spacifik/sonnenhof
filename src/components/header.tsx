e080456a92b049e79629fe48d069fe2d892ce769
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-05-13 10:15:24 +0200

    main: add cityid

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -85,7 +85,9 @@ export function Header(): JSX.Element {
           className="hidden md:flex md:gap-3 md:pt-3 justify-end"
           method="GET"
           action="https://app.mews.com/distributor/9c4dca19-3942-46a3-8f37-affe006f4062"
+          target="_blank"
         >
+          <input className="hidden" name="mewsCityId" value="c4d8e6d4-da08-40f5-b95e-affe006ea0ef" />
           <DatePicker
             selected={dates.from}
             onChange={(newDate) => {
