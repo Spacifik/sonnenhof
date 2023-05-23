bb8a074c677ba16942213a0fd46669627faa0101
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-05-23 14:42:21 +0200

    main: add locale detection

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "out"
+  distDir: "out",
+  i18n: {
+    locales: ["de"],
+    defaultLocale: "de",
+  }
 }
 
 module.exports = nextConfig
