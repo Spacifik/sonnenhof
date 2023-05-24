13da09a56fd8fb3d3ffbc7e90bf0aff00cf042bf
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-05-24 15:55:46 +0200

    main: add background brightenign

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -31,7 +31,7 @@ export function Section({
   return (
     <section
       className={`
-      grow relative min-h-40vh p-12 md:p-8 md:basis-1/2 md:min-h-60vh flex
+      grow relative min-h-40vh md:basis-1/2 md:min-h-60vh flex
       ${details ? "group md:hover:brightness-150" : ""}
       ${background ? "bg-cover bg-no-repeat" : "bg-black"} 
       `}
@@ -47,55 +47,57 @@ export function Section({
         }
       }, [buttons, router])}
     >
-      <div className="flex z-10 align-middle items-center justify-center ">
-        <div className="opacity-100 group-hover:opacity-0 transition-all flex flex-col gap-6">
-          {hint && (
-            <div>
-              <Text variant={`tiny-${textColorVariant}`} mono uppercase>
-                {hint}
-              </Text>
-            </div>
-          )}
-          <Text variant={`big-${textColorVariant}`} mono uppercase>
-            {title}
-          </Text>
-          {(props as SectionProps).text ? (
-            <Text variant={`small-${textColorVariant}`}>
-              {(props as SectionProps).text}
+      <div className=" backdrop-brightness-200  p-12 md:p-8  w-full">
+        <div className="flex z-10 align-middle items-center justify-center">
+          <div className="opacity-100 group-hover:opacity-0 transition-all flex flex-col gap-6">
+            {hint && (
+              <div>
+                <Text variant={`tiny-${textColorVariant}`} mono uppercase>
+                  {hint}
+                </Text>
+              </div>
+            )}
+            <Text variant={`big-${textColorVariant}`} mono uppercase>
+              {title}
             </Text>
-          ) : (
-            <>
-              {
-                (props as React.PropsWithChildren<Omit<SectionProps, "text">>)
-                  .children
-              }
-            </>
-          )}
-        </div>
-      </div>
-      {details ? (
-        <div className="absolute inset-0 items-center justify-center z-20 align-middle bg-transparent opacity-0 w-full h-full p-4  md:p-8 transition-all group-hover:md:opacity-100 group-hover:pointer-events:none">
-          {hint && (
-            <div className="pb-2 md:pb-5 lg:pb-8">
-              <Text variant={`medium-${textColorVariant}`} mono uppercase>
-                {hint}
+            {(props as SectionProps).text ? (
+              <Text variant={`small-${textColorVariant}`}>
+                {(props as SectionProps).text}
               </Text>
-            </div>
-          )}
-          <Text variant="small-primary">{details}</Text>
-          <div className="flex gap-4 pt-3 md:pt-5">
-            {buttons.map(({ href, ...button }) =>
-              href ? (
-                <Link className="cursor-pointer" key={href} href={href}>
-                  <Button key={button.label} {...button} />
-                </Link>
-              ) : (
-                <Button key={button.label} {...button} />
-              )
+            ) : (
+              <>
+                {
+                  (props as React.PropsWithChildren<Omit<SectionProps, "text">>)
+                    .children
+                }
+              </>
             )}
           </div>
         </div>
-      ) : null}
+        {details ? (
+          <div className="absolute inset-0 items-center justify-center z-20 align-middle bg-transparent opacity-0 w-full h-full p-4  md:p-8 transition-all group-hover:md:opacity-100 group-hover:pointer-events:none">
+            {hint && (
+              <div className="pb-2 md:pb-5 lg:pb-8">
+                <Text variant={`medium-${textColorVariant}`} mono uppercase>
+                  {hint}
+                </Text>
+              </div>
+            )}
+            <Text variant="small-primary">{details}</Text>
+            <div className="flex gap-4 pt-3 md:pt-5">
+              {buttons.map(({ href, ...button }) =>
+                href ? (
+                  <Link className="cursor-pointer" key={href} href={href}>
+                    <Button key={button.label} {...button} />
+                  </Link>
+                ) : (
+                  <Button key={button.label} {...button} />
+                )
+              )}
+            </div>
+          </div>
+        ) : null}
+      </div>
     </section>
   );
 }
