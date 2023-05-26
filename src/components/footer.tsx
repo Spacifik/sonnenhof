dd8a0fb4c148d77300c00adc00635924433aafae
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-05-26 15:12:40 +0200

    main: increase icon size on huge screens

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -77,7 +77,11 @@ function MediaIconWrapper({
 }: React.PropsWithChildren<{ className?: string }>): JSX.Element {
   return (
     <div
-      className={`${className} md:hover:brightness-150 cursor-pointer w-12 h-12 text-2xl rounded-full flex items-center justify-center text-primary-regular`}
+      className={`${className} md:hover:brightness-150 cursor-pointer rounded-full flex items-center justify-center text-primary-regular
+      text-2xl w-12 h-12 
+      xl:text-3xl w-14 h-14
+      2xl:text-4xl  w-20 h-16
+      `}
     >
       {children}
     </div>
