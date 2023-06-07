c031be29eb498a87b958877824e516d5694c95e9
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-06-07 08:01:33 +0200

    main: add max h 100svh

--- src/pages/rooms/index.tsx
+++ src/pages/rooms/index.tsx
@@ -29,7 +29,7 @@ export default function Rooms(): JSX.Element {
         <div>
           <Image
             name="rooms-overview"
-            className="bg-cover bg-no-repeat object-cover w-full min-h-screen center"
+            className="bg-cover bg-no-repeat object-cover w-full min-h-screen max-h-[100svh] center"
           />
         </div>
         <section className="flex flex-col gap-7 md:gap-10 xl:gap-16 2xl:gap-20 m-3 md:m-5 pt-3 md:pt-5 lg:pt-10 2xl:pt-16 pb-3 2xl:pb-10 lg:m-10 xl:m-auto lg:max-w-[80vw] xl:max-w-[70vw] 2xl:max-w-[60vw]">
